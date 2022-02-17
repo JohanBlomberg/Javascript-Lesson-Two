e868dd67828ff6b5288f09cc3200aa0d5251277b
Johan Blomberg <Johanblomberg89@gmail.com>
2022-02-17 15:38:36 +0100

    Separated functions

--- Exercise One/Javascript/Script.js
+++ Exercise One/Javascript/Script.js
@@ -1,16 +1,23 @@
-
-function displayCurrentDate () {
-let weekdays = ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'];
-let month = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'Oktober', 'November', 'December']
-
 let date = new Date();
-let currentDay = weekdays[date.getDay()];
+
+let translateDays = () => {
+    let weekdays = ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'];
+        return weekdays[date.getDay()];
+}
+
+let translateMonths = () => {
+    let month = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'Oktober', 'November', 'December'];
+      return month[date.getMonth()];
+}
+
+let displayCurrentDate = () => {
 let currentDate = date.getDate();
-let currentMonth = month[date.getMonth()];
 let currentYear = date.getFullYear();
+let displayDay = translateDays();
+let displayMonth = translateMonths();
 
 let displayDateHTML = `
-Idag är det ${currentDay} ${currentDate} ${currentMonth} ${currentYear}
+Idag är det ${displayDay} ${currentDate} ${displayMonth} ${currentYear}
 `
 
 document.getElementById('displayDate').innerHTML = displayDateHTML;
